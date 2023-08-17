f36a95780659e6fa4d880ba95984263f028379b6
Szymzal <szymzal05@gmail.com>
2023-08-17 11:32:40 +0200

    Modulize

--- public/modpacks/TrueSurvival/groovy/postInit/OreDictionary/OreDict-Chisel.groovy
+++
@@ -1,9 +0,0 @@
-
-
-if (isLoaded("chisel")) {
-
-    // --- Добавление
-
-    // Стамески
-    ore('hiChisel').add(item('chisel:chisel_iron'), item('chisel:chisel_diamond'), item('chisel:chisel_hitech'))
-}
